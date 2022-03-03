e77e16916d76c58da50a7fcb14d1bdade668f5bd
mo256man <48610559+mo256man@users.noreply.github.com>
2022-03-03 23:51:08 +0900

    Add files via upload

--- show_history.py
+++ show_history.py
@@ -3,8 +3,12 @@ import shutil
 import sqlite3
 from contextlib import closing
 from datetime import datetime
+import urllib.parse
+import requests
+import json
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
-print(os.environ)
 user_path = os.environ["LOCALAPPDATA"]
 db = user_path + r"\Google\Chrome\User Data\Default\History"
 db_copy = db + "_copy"
@@ -13,18 +17,32 @@ if os.path.exists(db_copy):
     os.remove(db_copy)
 shutil.copyfile(db, db_copy)
 
-filePath = 'result.txt'
+terms = ""
 
 with closing(sqlite3.connect(db_copy)) as conn:
     c = conn.cursor()
-    select_sql = "select visits.id, urls.url, urls.title, visits.visit_time, visits.from_visit from visits inner join urls on visits.url = urls.id"
-    with open(filePath, mode="w", encoding="utf-8") as f:
-        for row in c.execute(select_sql):
-            id , url, title, visit_time, from_visit = row
-            timestamp = datetime.fromtimestamp(visit_time/1000000-11644473600)
-            if "Google 検索" in title:
-                title = title[:-len(" - Google 検索")]
-                print(title)
-                f.write(f"{id}, {url}, {title}, {timestamp}, {from_visit}\n")
-        f.close()
 
+    sqls = ["select term from keyword_search_terms", "select title from urls"]
+    for sql in sqls:
+        recordset = c.execute(sql)
+        for row in recordset:
+            terms += " " + row[0]
+
+terms = urllib.parse.unquote(terms)
+
+app_id = "dj00aiZpPXo5cG9DYkh6Qm93byZzPWNvbnN1bWVyc2VjcmV0Jng9YTY-"
+headers = {"Content-Type": "application/json",
+           "User-Agent": 'Yahoo AppID: {0}'.format(app_id)}
+parameters = {"app_id": app_id,  
+              "sentence": "庭には二羽ニワトリがいる。", 
+              "results":"ma, uniq"}
+
+response = requests.post("https://jlp.yahooapis.jp/MAService/V1/parse",headers=headers, data=parameters)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup)
+else:
+    print(f"error: status_code={response.status_code}")
+
+pass
